6069169df11822d6f1be060b931dabb9fd47bba6
TynK-M <matteo.pauroso@gmail.com>
2026-01-17 17:42:02 +0100

    feat: add more controls and flag -d
    Added more controls to ensure the path exists and that the files are not empty, also added the flag for debug printing(-d and --debug)

--- src/compare/compare.py
+++ src/compare/compare.py
@@ -1,22 +1,43 @@
 import typer
+import os
 from typing_extensions import Annotated
 from pathlib import Path
 from rich.console import Console
 
+console = Console()
 err_console = Console(stderr=True)
 
 def compare(
         first_csv: Annotated[Path, typer.Argument(help="Path to the first CSV file")] = None,
         second_csv: Annotated[Path, typer.Argument(help="Path to the second CSV file")] = None,
         fcsv: Annotated[Path | None, typer.Option("-fcsv", "--first-csv", help="Optional flag to specify the first CSV file")] = None,
-        scsv: Annotated[Path | None, typer.Option("-scsv", "--second-csv", help="Optional flag to specify the second CSV file")] = None
+        scsv: Annotated[Path | None, typer.Option("-scsv", "--second-csv", help="Optional flag to specify the second CSV file")] = None,
+        debug: Annotated[bool, typer.Option("-d", "--debug", help="Optional flag to enable debug printing")] = False
 ) -> None:
     first_csv = fcsv or first_csv
     second_csv = scsv or second_csv
 
-    if not first_csv or not second_csv:
+    if not first_csv or not second_csv \
+        or not os.path.isfile(first_csv) or not os.path.isfile(second_csv):
         err_console.print("[bold red]Error: you must provide two CSV files, either positionally or via flags[/bold red]")
         raise typer.Exit(code=1)
 
-    print(f"Comparing {first_csv} with {second_csv}")
+    console.print(f"[bold green]Comparing `{first_csv}` with `{second_csv}`...[/bold green]")
+
+    first_csv_content = None
+    second_csv_content = None
+
+    with open(first_csv, 'r') as first_csv_file:
+        first_csv_content = first_csv_file.read()
+
+    with open(second_csv, 'r') as second_csv_file:
+        second_csv_content = second_csv_file.read()
+
+    if not first_csv_content or not second_csv_content:
+        err_console.print("[bold red]Error: could not compare when one of the files is empty[/bold red]")
+        raise typer.Exit(code=1)
+
+    if debug:
+        console.print(f"First CSV content:\n[yellow]{first_csv_content}[/yellow]")
+        console.print(f"Second CSV content:\n[yellow]{second_csv_content}[/yellow]")
 
